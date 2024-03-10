0c9fd7bd45807060daa298ce9a360b75b3a52834
Secelia McNair <secelia@yahoo.com>
2024-03-10 18:46:50 -0500

    added variables for rgb

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let button = document.querySelector("button");
 
 
 function backgroundColorChanges() {
-  let number = Math.floor(Math.random() * 255);
-  document.body.style.backgroundColor = `rgb(${number} ${number} ${number})`;
+  let number1 = Math.floor(Math.random() * 255);
+  let number2 = Math.floor(Math.random() * 255);
+  let number3 = Math.floor(Math.random() * 255);
+  document.body.style.backgroundColor = `rgb(${number1} ${number2} ${number3})`;
 }
 
 button.addEventListener("click", backgroundColorChanges)
