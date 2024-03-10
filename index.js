ed7c01dae135724c84af3cee89c436ed5aacde85
Secelia McNair <secelia@yahoo.com>
2024-03-10 12:59:10 -0500

    updates made

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const min = 0;
-const max = 255;
+let button = document.querySelector("button");
 
-let randomNum1 = Math.floor(Math.random() * max) + min;
-let randomNum2 = Math.floor(Math.random() * max) + min;
-let randomNum3 = Math.floor(Math.random() * max) + min;
 
-function newBackground() {
-  let changeColor = document.getElementById("changeColor")
-  changeColor.style.backgroundColor = `rgb(${randomNum1}, ${randomNum2}, ${randomNum3})`
+function backgroundColorChanges() {
+  let number = Math.floor(Math.random() * 255);
+  document.body.style.backgroundColor = `rgb(${number} ${number} ${number})`;
 }
 
-//Come back to this code to figure out how to make a loop to continue changing the color
+button.addEventListener("click", backgroundColorChanges)
+
